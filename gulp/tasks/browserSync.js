0c5323a6da9ac0c347d8dfc40de665bc8aef5ef0
Dylan Grant <dgrant@hollandamerica.com>
2015-11-27 16:04:07 -0800

    added a server config to help with all urls

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var config      = require('../config');
-var browserSync = require('browser-sync');
-var gulp        = require('gulp');
+var config              = require('../config');
+var browserSync         = require('browser-sync');
+var gulp                = require('gulp');
+var historyApiFallback  = require('connect-history-api-fallback');
 
 gulp.task('browserSync', function() {
     //If I ever decide to set up an express server
@@ -12,7 +13,8 @@ gulp.task('browserSync', function() {
 
     browserSync.init({
         server: {
-            baseDir: "./build"
+            baseDir: "./build",
+            middleware: [ historyApiFallback() ]
         }
     });
 });
